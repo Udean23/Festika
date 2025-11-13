2774923bfddc47108c679f4cb2765d2596975877
Udean23 <ahmadsyarifudinnajihulw@gmail.com>
2025-11-13 18:06:49 +0700

    Feat: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Landing Page/**/*.html",
+    "./Tabel Periodik/**/*.html",
+    "./Tata Surya/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
