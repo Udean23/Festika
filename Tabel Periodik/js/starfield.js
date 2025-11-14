0d00142b475bc9e825ee0734e55cee3ab2528649
Udean23 <ahmadsyarifudinnajihulw@gmail.com>
2025-11-14 10:10:56 +0700

    Fix:Starfield

---
+++ Tabel Periodik/js/starfield.js
@@ -0,0 +1,13 @@
+const starfield = document.getElementById("starfield");
+for (let i = 0; i < 200; i++) {
+    const star = document.createElement("div");
+    star.className = "star";
+    const size = Math.random() * 2 + 0.5;
+    star.style.width = size + "px";
+    star.style.height = size + "px";
+    star.style.left = Math.random() * 100 + "%";
+    star.style.top = Math.random() * 100 + "%";
+    star.style.animationDelay = Math.random() * 3 + "s";
+    star.style.animationDuration = Math.random() * 2 + 2 + "s";
+    starfield.appendChild(star);
+}
